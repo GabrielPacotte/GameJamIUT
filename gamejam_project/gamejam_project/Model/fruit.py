0e65ae784303696d3f98d93fc0c4c89a49351a96
gueribas <guerin.bastien38@gmail.com>
2022-02-07 13:54:40 +0100

    fruitsù

--- gamejam_project/gamejam_project/Model/fruit.py
+++ gamejam_project/gamejam_project/Model/fruit.py
@@ -1,9 +1,11 @@
-from Point import Point
+import pygame
+import random
+from Model.Point import Point
 
 
 class Fruit:
 
-    def __init__(self, name, energy, point: Point, image):
+    def __init__(self, name, energy, point: Point):
         self.name = name
         self.energy = energy
         self.point = point
@@ -14,12 +16,34 @@ class Fruit:
     def getenerergy(self) -> str:
         return self.energy
 
-    def getpoint(self) -> Point :
+    def setenergy(self, valeur):
+        self.energy = valeur
+
+    def getpoint(self) -> Point:
         return self.point
 
+    def inHitBox(self, point: Point) -> bool:
+        if self.getpoint().getx() <= point.getx() <= self.getpoint().getx() + 20 and self.getpoint().gety() <= point.gety() <= self.getpoint().gety() + 20:
+            return True
+        else:
+            return False
 
+    def drawfruit(self, window):
+        x = random.randint(0, 10)
+        if x <= 5:
+            FRUITS_IMG = pygame.image.load("../img/citron.png")
+            FRUITS_IMG = pygame.transform.scale(FRUITS_IMG, (20, 20))
+            self.setenergy(1)
+        if 5 < x <= 8:
+            FRUITS_IMG = pygame.image.load("../img/pomme.png")
+            FRUITS_IMG = pygame.transform.scale(FRUITS_IMG, (20, 20))
+            self.setenergy(2)
+        if x > 8:
+            FRUITS_IMG = pygame.image.load("../img/banane.png")
+            FRUITS_IMG = pygame.transform.scale(FRUITS_IMG, (20, 20))
+            self.setenergy(3)
 
-
+        window.blit(FRUITS_IMG, (self.point.x, self.point.y))
 
 
 
---
+++ gamejam_project/gamejam_project/test_fruit.py
@@ -0,0 +1,29 @@
+# Main file
+import pygame
+from Model.Fruit import Fruit
+from Model.Point import Point
+
+pygame.init()
+
+WIN_WIDTH, WIN_HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+CURSOR = pygame.image.load("img/cursor.png")
+pygame.mouse.set_visible(False)  # hide the cursor
+
+#write this in the loop
+
+player = Player("Gab", 3, Point(100, 100))
+
+if __name__ == '__main__':
+
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+        player.move()
+
+        WIN.fill((0, 0, 0))
+        player.draw(WIN)
+        coord = pygame.mouse.get_pos()
+        WIN.blit(CURSOR, coord)
+        pygame.display.update()
+        pygame.event.pump()
