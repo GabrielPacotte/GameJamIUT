4e56a4b621e82f1260f181a27ddcf8cdd80ef653
gueribas <guerin.bastien38@gmail.com>
2022-02-07 14:27:21 +0100

    test fruits

--- gamejam_project/gamejam_project/test_fruit.py
+++ gamejam_project/gamejam_project/test_fruit.py
@@ -1,29 +1,41 @@
 # Main file
 import pygame
+import random
 from Model.Fruit import Fruit
 from Model.Point import Point
+#from Model.Player import Player
+
 
 pygame.init()
 
 WIN_WIDTH, WIN_HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-CURSOR = pygame.image.load("img/cursor.png")
-pygame.mouse.set_visible(False)  # hide the cursor
 
 #write this in the loop
 
-player = Player("Gab", 3, Point(100, 100))
+#player = Player("Gab", 3, Point(100, 100))
+
+x = random.randint(0, 780)
+y = random.randint(0, 580)
+fruit = Fruit("fruit", 1, Point(x, y))
+fruit.drawfruit(WIN)
+pygame.display.update()
+
+time_Before = pygame.time.get_ticks()
 
 if __name__ == '__main__':
 
     clock = pygame.time.Clock()
     while True:
         clock.tick(60)
-        player.move()
+        time = pygame.time.get_ticks() - time_Before
+        print(time)
+        if time > 2000:
+            x = random.randint(0, 780)
+            y = random.randint(0, 580)
+            fruit = Fruit("fruit", 1, Point(x, y))
+            fruit.drawfruit(WIN)
+            pygame.display.update()
+            time_Before = pygame.time.get_ticks()
 
-        WIN.fill((0, 0, 0))
-        player.draw(WIN)
-        coord = pygame.mouse.get_pos()
-        WIN.blit(CURSOR, coord)
         pygame.display.update()
-        pygame.event.pump()
