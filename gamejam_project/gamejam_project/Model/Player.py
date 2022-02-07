7d293e47a08a0c044e9a8d9984c4d95972a5510b
pacotteg <pacotteg@etu.univ-grenoble-alpes.fr>
2022-02-07 13:12:39 +0100

    Ajout des animations et du déplacement du joueur

--- gamejam_project/gamejam_project/Model/Player.py
+++ gamejam_project/gamejam_project/Model/Player.py
@@ -1,8 +1,28 @@
 import pygame
 from Model.Point import Point
 
-PLAYER_IMG = pygame.image.load("img/player.png")
-PLAYER_IMG = pygame.transform.scale(PLAYER_IMG, (50, 70))
+moveDownSprites = []
+moveUpSprites = []
+moveRightSprites = []
+moveLeftSprites = []
+
+for i in range(1, 6):
+    moveDownSprites.append(pygame.image.load("img/player_move_down/player_move_down_" + str(i) + ".png"))
+    moveDownSprites[i-1] = pygame.transform.scale(moveDownSprites[i-1], (42, 63))
+
+for i in range(1, 6):
+    moveUpSprites.append(pygame.image.load("img/player_move_up/player_move_up_" + str(i) + ".png"))
+    moveUpSprites[i-1] = pygame.transform.scale(moveUpSprites[i-1], (42, 63))
+
+for i in range(1, 6):
+    moveRightSprites.append(pygame.image.load("img/player_move_right/player_move_right_" + str(i) + ".png"))
+    moveRightSprites[i-1] = pygame.transform.scale(moveRightSprites[i-1], (42, 63))
+
+for i in range(1, 6):
+    moveLeftSprites.append(pygame.image.load("img/player_move_left/player_move_left_" + str(i) + ".png"))
+    moveLeftSprites[i-1] = pygame.transform.scale(moveLeftSprites[i-1], (42, 63))
+
+#PLAYER_IMG = moveDownSprites[0]
 
 class Player:
 
@@ -10,12 +30,40 @@ class Player:
         self.name = name
         self.moveSpeed = moveSpeed
         self.point = point
+        self.currentSprite = 0;
+        self.player_img = moveDownSprites[0]
 
     def move(self):
         keys = pygame.key.get_pressed()
         self.point.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * self.moveSpeed
         self.point.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * self.moveSpeed
+        self.animate()
 
+    def animate(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]: # Animate down movement
+            self.player_img = moveDownSprites[int(self.currentSprite)]
+            self.currentSprite += 0.15
+            if self.currentSprite >= len(moveDownSprites):
+                self.currentSprite = 0
+        elif keys[pygame.K_UP]:  # Animate up movement
+            self.player_img = moveUpSprites[int(self.currentSprite)]
+            self.currentSprite += 0.15
+            if self.currentSprite >= len(moveUpSprites):
+                self.currentSprite = 0
+        elif keys[pygame.K_LEFT]:
+            self.player_img = moveLeftSprites[int(self.currentSprite)]
+            self.currentSprite += 0.15
+            if self.currentSprite >= len(moveLeftSprites):
+                self.currentSprite = 0
+        elif keys[pygame.K_RIGHT]:
+            self.player_img = moveRightSprites[int(self.currentSprite)]
+            self.currentSprite += 0.15
+            if self.currentSprite >= len(moveRightSprites):
+                self.currentSprite = 0
+        else:
+            self.currentSprite = 0
+            self.player_img = moveDownSprites[int(self.currentSprite)]
 
     def draw(self, window):
-        window.blit(PLAYER_IMG, (self.point.x, self.point.y))
+        window.blit(self.player_img, (self.point.x, self.point.y))
--- gamejam_project/gamejam_project/main.py
+++ gamejam_project/gamejam_project/main.py
@@ -9,7 +9,7 @@ pygame.init()
 WIN_WIDTH, WIN_HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-player = Player("Gab", 2, Point(100, 100))
+player = Player("Gab", 3, Point(100, 100))
 
 if __name__ == '__main__':
 
@@ -17,6 +17,8 @@ if __name__ == '__main__':
     while True:
         clock.tick(60)
         player.move()
+
+        WIN.fill((255, 255, 255))
         player.draw(WIN)
         #player.point.x += 1
         pygame.display.update()
